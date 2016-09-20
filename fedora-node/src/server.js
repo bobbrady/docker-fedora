3e8239e2135303c246125a6d0d645ec776799752
Bob Brady <bob@digibrady.com>
2016-09-20 14:27:11 -0400

    File cleanup

--- fedora-node/src/server.js
+++ fedora-node/src/server.js
@@ -2,13 +2,11 @@
 
 const express = require('express');
 
-// Constants
 const PORT = process.env.PORT || 3000;
 
-// App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello FUBAR world\n');
+  res.send('Hello World!');
 });
 
 app.listen(PORT);
